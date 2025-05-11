e1bccb49fe28764c2a9279e7106c22e43bbf3694
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-05-11 21:15:55 +0800

    add test user show

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Admin, Resource, ListGuesser, EditGuesser, Show } from "react-admin";
+import { Admin, Resource, EditGuesser } from "react-admin";
 import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
 import { UserList } from "./pages/users/user-list";
+import { UserShow } from "./pages/users/user-show";
 
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider}>
-    <Resource name="posts" list={ListGuesser} edit={EditGuesser} show={Show} />
-    <Resource name="users" list={UserList} edit={EditGuesser} show={Show} />
+    <Resource name="users" list={UserList} edit={EditGuesser} show={UserShow} />
   </Admin>
 );
