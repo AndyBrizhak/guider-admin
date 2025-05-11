72b41fa3fc44649c9501468fbc83137510ed73e1
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-05-11 21:56:17 +0800

    add filters and icon for test users

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,16 @@ import { dataProvider } from "./dataProvider";
 import { UserList } from "./pages/users/user-list";
 import { UserShow } from "./pages/users/user-show";
 import { UserEdit } from "./pages/users/user-edit";
+import PersonIcon from "@mui/icons-material/Person";
 
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider}>
-    <Resource name="users" list={UserList} edit={UserEdit} show={UserShow} />
+    <Resource
+      icon={PersonIcon}
+      name="users"
+      list={UserList}
+      edit={UserEdit}
+      show={UserShow}
+    />
   </Admin>
 );
--- src/pages/users/user-list.tsx
+++ src/pages/users/user-list.tsx
@@ -1,17 +1,32 @@
-import { Datagrid, EmailField, List, TextField, EditButton } from "react-admin";
+import {
+  Datagrid,
+  EmailField,
+  List,
+  TextField,
+  EditButton,
+  TextInput,
+} from "react-admin";
 
-export const UserList = () => (
-  <List>
-    <Datagrid>
-      <TextField source="id" />
-      <TextField source="name" />
-      <TextField source="username" />
-      <EmailField source="email" />
-      <TextField source="address.street" />
-      <TextField source="phone" />
-      <TextField source="website" />
-      <TextField source="company.name" />
-      <EditButton />
-    </Datagrid>
-  </List>
-);
+const userFilters = [
+  <TextInput source="q" label="Search" alwaysOn />,
+  <TextInput source="name" label="Name" />,
+  <TextInput source="username" label="Username" />,
+  <TextInput source="email" label="Email" />,
+];
+export const UserList = () => {
+  return (
+    <List filters={userFilters}>
+      <Datagrid>
+        <TextField source="id" />
+        <TextField source="name" />
+        <TextField source="username" />
+        <EmailField source="email" />
+        <TextField source="address.street" />
+        <TextField source="phone" />
+        <TextField source="website" />
+        <TextField source="company.name" />
+        <EditButton />
+      </Datagrid>
+    </List>
+  );
+};
