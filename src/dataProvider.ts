5ec24c22138461a96ce000e39ecccf937c6514f2
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-05-17 12:42:51 +0800

    enable passing access token in requests from data provider

--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -1,11 +1,20 @@
 import { DataProvider, fetchUtils } from "react-admin";
-// import { UserRecord } from "./types";
 
 const API_URL = import.meta.env.VITE_API_URL || "https://localhost:7078";
-// export const dataProvider = jsonServerProvider(
-//   import.meta.env.VITE_JSON_SERVER_URL ||
-//     "https://jsonplaceholder.typicode.com",
-// );
+
+// Кастомный httpClient для добавления токена авторизации
+const httpClient = (url: string, options: fetchUtils.Options = {}) => {
+  if (!options.headers) {
+    options.headers = new Headers({ Accept: "application/json" });
+  }
+  const auth = localStorage.getItem("auth");
+  if (auth) {
+    const { token } = JSON.parse(auth);
+    options.headers.set("Authorization", `Bearer ${token}`);
+  }
+
+  return fetchUtils.fetchJson(url, options);
+};
 
 export const dataProvider: DataProvider = {
   getList: async (resource, params) => {
@@ -28,7 +37,7 @@ export const dataProvider: DataProvider = {
     ).toString();
     const url = `${API_URL}/${resource}?${queryString}`;
 
-    const response = await fetchUtils.fetchJson(url);
+    const response = await httpClient(url);
 
     if (response.status < 200 || response.status >= 300) {
       throw new Error(`Error fetching ${resource}: ${response.body}`);
@@ -41,22 +50,22 @@ export const dataProvider: DataProvider = {
   },
   getOne: async (resource, params) => {
     const url = `${API_URL}/${resource}/${params.id}`;
-    const response = await fetchUtils.fetchJson(url);
+    const response = await httpClient(url);
     return { data: await response.json };
   },
   getMany: async (resource, params) => {
     const url = `${API_URL}/${resource}?id=${params.ids.join(",")}`;
-    const response = await fetchUtils.fetchJson(url);
+    const response = await httpClient(url);
     return { data: await response.json };
   },
   getManyReference: async (resource, params) => {
     const url = `${API_URL}/${resource}?${params.target}=${params.id}`;
-    const response = await fetchUtils.fetchJson(url);
+    const response = await httpClient(url);
     return { data: await response.json, total: response.json.length };
   },
   update: async (resource, params) => {
     const url = `${API_URL}/${resource}/${params.id}`;
-    const response = await fetchUtils.fetchJson(url, {
+    const response = await httpClient(url, {
       method: "PUT",
       body: JSON.stringify(params.data),
     });
@@ -65,7 +74,7 @@ export const dataProvider: DataProvider = {
   updateMany: async (resource, params) => {
     const responses = await Promise.all(
       params.ids.map((id) =>
-        fetchUtils.fetchJson(`${API_URL}/${resource}/${id}`, {
+        httpClient(`${API_URL}/${resource}/${id}`, {
           method: "PUT",
           body: JSON.stringify(params.data),
         }),
@@ -75,7 +84,7 @@ export const dataProvider: DataProvider = {
   },
   create: async (resource, params) => {
     const url = `${API_URL}/${resource}`;
-    const response = await fetchUtils.fetchJson(url, {
+    const response = await httpClient(url, {
       method: "POST",
       body: JSON.stringify(params.data),
     });
@@ -83,13 +92,13 @@ export const dataProvider: DataProvider = {
   },
   delete: async (resource, params) => {
     const url = `${API_URL}/${resource}/${params.id}`;
-    const response = await fetchUtils.fetchJson(url, { method: "DELETE" });
+    const response = await httpClient(url, { method: "DELETE" });
     return { data: response.json };
   },
   deleteMany: async (resource, params) => {
     const responses = await Promise.all(
       params.ids.map((id) =>
-        fetchUtils.fetchJson(`${API_URL}/${resource}/${id}`, {
+        httpClient(`${API_URL}/${resource}/${id}`, {
           method: "DELETE",
         }),
       ),
