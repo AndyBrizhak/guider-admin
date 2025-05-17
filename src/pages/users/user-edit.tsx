57e30f61d1481722dbb9e5384360e6c79dbcdf9c
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-05-17 13:46:38 +0800

    add select input for roles users

--- src/pages/users/user-edit.tsx
+++ src/pages/users/user-edit.tsx
@@ -1,11 +1,18 @@
-import { Edit, SimpleForm, TextInput } from "react-admin";
+import { Edit, SimpleForm, TextInput, SelectInput } from "react-admin";
+
+const roleChoices = [
+  { id: "user", name: "user" },
+  { id: "manager", name: "manager" },
+  { id: "admin", name: "admin" },
+  { id: "superadmin", name: "superadmin" },
+];
 
 export const UserEdit = () => (
   <Edit>
     <SimpleForm>
       <TextInput source="username" />
       <TextInput source="email" />
-      <TextInput source="role" />
+      <SelectInput source="role" choices={roleChoices} />
     </SimpleForm>
   </Edit>
 );
