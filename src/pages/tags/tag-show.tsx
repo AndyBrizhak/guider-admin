446ced08db938dd50b3957b330dbf38cd3a4249e
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-06-06 09:08:06 +0800

    upgrade interface for tags

--- src/pages/tags/tag-show.tsx
+++ src/pages/tags/tag-show.tsx
@@ -3,7 +3,6 @@ import { Show, SimpleShowLayout, TextField } from "react-admin";
 export const TagShow = () => (
   <Show>
     <SimpleShowLayout>
-      <TextField source="id" />
       <TextField source="name_en" label="English Name" />
       <TextField source="name_sp" label="Spanish Name" />
       <TextField source="url" label="URL Slug" />
