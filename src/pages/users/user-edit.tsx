1cb474609002afe5d32979db65e171732e1758d5
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-06-06 08:49:06 +0800

    upgrade interface for edit users

--- src/pages/users/user-edit.tsx
+++ src/pages/users/user-edit.tsx
@@ -1,4 +1,4 @@
-import { Edit, SimpleForm, TextInput, SelectInput } from "react-admin";
+import { Edit, WizardForm, TextInput, SelectInput } from "react-admin";
 
 const roleChoices = [
   { id: "user", name: "user" },
@@ -9,7 +9,7 @@ const roleChoices = [
 
 export const UserEdit = () => (
   <Edit>
-    <SimpleForm
+    <WizardForm
       style={{
         width: "75vw",
         maxWidth: "75vw",
@@ -19,9 +19,18 @@ export const UserEdit = () => (
         gap: 24,
       }}
     >
-      <TextInput source="username" label="Username" fullWidth />
-      <TextInput source="email" label="Email" fullWidth />
-      <SelectInput source="role" label="Role" choices={roleChoices} fullWidth />
-    </SimpleForm>
+      <WizardForm.Step label="User Info">
+        <TextInput source="username" label="Username" fullWidth />
+        <TextInput source="email" label="Email" fullWidth />
+      </WizardForm.Step>
+      <WizardForm.Step label="Role">
+        <SelectInput
+          source="role"
+          label="Role"
+          choices={roleChoices}
+          fullWidth
+        />
+      </WizardForm.Step>
+    </WizardForm>
   </Edit>
 );
