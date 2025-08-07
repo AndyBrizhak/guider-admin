cd5263c4eed4f9f7f14d485f8db9b20afba32fea
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2025-08-07 10:22:45 +0800

    setup localhost and port 3000

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     host: true,
+    port: 3000, // Запуск локального сервера на порту 3000
   },
   build: {
     sourcemap: mode === "development",
